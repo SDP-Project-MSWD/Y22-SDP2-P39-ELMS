97d8a6e57a81e4bbcae7569acac66819c80406bb
klu-2200090053 <2200090053@kluniversity.in>
2024-03-09 19:17:19 +0530

    Dashboards

--- leave-module-frontend/src/App.js
+++ leave-module-frontend/src/App.js
@@ -7,6 +7,12 @@ import SignIn from './Components/Login/SignIn';
 import ForgotPassword from "./Components/Login/ForgotPassword";
 import ResetPassword from './Components/Login/ResetPassword';
 
+
+import HomeA from './Components/Admin/HomeA';
+import HomeM from './Components/User/Manager/HomeM';
+import HomeTL from './Components/User/Team Lead/HomeTL';
+import HomeE from './Components/User/Employee/HomeE';
+
 function App() {
   //Leave management system
   return (
@@ -18,6 +24,10 @@ function App() {
         <Route path='signIn' element={<SignIn />} />
         <Route path='forgotPassword' element={<ForgotPassword />} />
         <Route path='reset-password/:id/:token' element={<ResetPassword />}/>
+        <Route path="admin" element={<HomeA />} />
+        <Route path="manager" element={<HomeM />} />
+        <Route path="teamLead" element={<HomeTL />} />
+        <Route path="employee" element={<HomeE />} />
       </Routes>
     </BrowserRouter>
     </AuthProvider>
---
+++ leave-module-frontend/src/Components/User/Team Lead/listItems.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+//Images Import Section
+import Dashboard from '../../Images/DashBoard.png';
+import Attendance from '../../Images/attendance.png';
+import Leave from '../../Images/leave.png';
+//import Settings from '../../Images/settings.png';
+import Profile from '../../Images/profile.png';
+
+
+export const mainListItems = (
+  <React.Fragment>
+    <ListItemButton component={RouterLink} to="/teamlead/dashboard">
+      <ListItemIcon>
+      <img src={Dashboard} alt='DashBoard Images'  className='avatar' style={{ width: '24px', height: '24px' }}/>
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItemButton>
+    
+    <ListItemButton component={RouterLink} to="/Home/attendance">
+      <ListItemIcon>
+        <img src={Attendance} alt='DashBoard Images' className='avatar' style={{ width: '24px', height: '24px' }}/>
+      </ListItemIcon>
+      <ListItemText primary="Attendance" />
+    </ListItemButton>
+    
+    <ListItemButton component={RouterLink} to="/Home/leaves">
+      <ListItemIcon>
+        <img src={Leave} alt='DashBoard Images' className='avatar' style={{ width: '24px', height: '24px' }}/>
+      </ListItemIcon>
+      <ListItemText primary="Leaves" />
+    </ListItemButton>
+
+    <ListItemButton component={RouterLink} to="/Home/profile">
+      <ListItemIcon>
+        <img src={Profile} alt='DashBoard Images' className='avatar' style={{ width: '24px', height: '24px' }}/>
+      </ListItemIcon>
+      <ListItemText primary="Profile" />
+    </ListItemButton>
+  </React.Fragment>
+);
