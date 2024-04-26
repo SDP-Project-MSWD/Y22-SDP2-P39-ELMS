1dd6673ce97ff03d2f7b0b0917b6ef8f808cafb5
LingaShashank <149906239+LingaShashank@users.noreply.github.com>
2024-04-27 04:41:10 +0530

    Update index.js

--- leave-module-backend/index.js
+++ leave-module-backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cors());
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://elms-frontend-beta.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 
